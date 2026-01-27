cc1e43fb2963e0f35b30d4d928c4b96f277716a7
PritamP20 <pripritam7@gmail.com>
2026-01-27 19:54:08 +0530

    feat: Implement D1 database integration and update dependencies for the upload API worker, and add new project setup files.

---
+++ processor/target/debug/deps/processor-7cb2599d211fa098.d
@@ -0,0 +1,5 @@
+/Users/pritam/Documents/Rust/Copy-right/processor/target/debug/deps/processor-7cb2599d211fa098.d: src/main.rs
+
+/Users/pritam/Documents/Rust/Copy-right/processor/target/debug/deps/libprocessor-7cb2599d211fa098.rmeta: src/main.rs
+
+src/main.rs:
---
+++ processor/target/debug/deps/processor-f15878aaea10fcb3.d
@@ -0,0 +1,5 @@
+/Users/pritam/Documents/Rust/Copy-right/processor/target/debug/deps/processor-f15878aaea10fcb3.d: src/main.rs
+
+/Users/pritam/Documents/Rust/Copy-right/processor/target/debug/deps/libprocessor-f15878aaea10fcb3.rmeta: src/main.rs
+
+src/main.rs:
