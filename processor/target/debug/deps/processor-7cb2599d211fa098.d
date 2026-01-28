499c577bc1b45a4a908db0935f034b170dea4e8b
PritamP20 <pripritam7@gmail.com>
2026-01-28 22:28:50 +0530

    chore: stop tracking target directory

--- processor/target/debug/deps/processor-7cb2599d211fa098.d
+++
@@ -1,5 +0,0 @@
-/Users/pritam/Documents/Rust/Copy-right/processor/target/debug/deps/processor-7cb2599d211fa098.d: src/main.rs
-
-/Users/pritam/Documents/Rust/Copy-right/processor/target/debug/deps/libprocessor-7cb2599d211fa098.rmeta: src/main.rs
-
-src/main.rs:
--- processor/target/debug/deps/processor-f15878aaea10fcb3.d
+++
@@ -1,5 +0,0 @@
-/Users/pritam/Documents/Rust/Copy-right/processor/target/debug/deps/processor-f15878aaea10fcb3.d: src/main.rs
-
-/Users/pritam/Documents/Rust/Copy-right/processor/target/debug/deps/libprocessor-f15878aaea10fcb3.rmeta: src/main.rs
-
-src/main.rs:
